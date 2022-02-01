3f5e57e02e0d51a90af61bc481d5d8405d23474b
NinoosMoshi <nalkaton@gmail.com>
2022-01-31 20:13:21 -0500

    create discovery service

--- discovery-service/src/main/java/com/ninos/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/ninos/DiscoveryServiceApplication.java
@@ -2,14 +2,14 @@ package com.ninos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
-@EnableEurekaClient
-public class BookingServiceApplication {
+public class DiscoveryServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookingServiceApplication.class, args);
+		SpringApplication.run(DiscoveryServiceApplication.class, args);
 	}
 
 }
